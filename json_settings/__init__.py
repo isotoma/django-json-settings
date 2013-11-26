9f4cc29300fd6d4b6fe642e7d9033ec0527c247d
Doug Winter <doug.winter@isotoma.com>
2013-11-26 11:37:14 +0000

    first simple implementation

---
+++ json_settings/__init__.py
@@ -0,0 +1,39 @@
+
+import sys
+import os
+import logging
+import json
+
+def json_patch(path):
+    logging.warn("Attempting to load local settings from %r" %(path,))
+    try:
+        d = json.load(open(path))
+    except IOError:
+        logging.exception("Unable to open json settings in %r" % (path,))
+        raise SystemExit(-1)
+    except ValueError:
+        logging.exception("Unable to parse json settings in %r" % (path,))
+        raise SystemExit(-1)
+    for k,v in d.items():
+        globals()[k] = v
+
+def patch_settings():
+    env_settings = os.environ.get('JSON_SETTINGS', None)
+    if env_settings is None:
+        # we only use the default if it exists
+        env_settings = os.path.join(sys.prefix, "etc", "settings.json")
+        if not os.path.exists(env_settings):
+            return
+    json_patch(env_settings)
+    if not "VAR_DIRECTORY" in globals():
+        globals()["VAR_DIRECTORY"] = os.path.join(sys.prefix, "var")
+    if not "STATIC_ROOT" in globals():
+        globals()["STATIC_ROOT"] = os.path.join(globals()["VAR_DIRECTORY"],
+                                                "static")
+    if not "MEDIA_ROOT" in globals():
+        globals()["MEDIA_ROOT"] = os.path.join(globals()["VAR_DIRECTORY"],
+                                               "media")
+        
+
+patch_settings()
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+README=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-json-settings",
+    version="0.1",
+    packages=["json_settings"],
+    include_package_data=True,
+    license="Apache Software License",
+    description="A Django application to let you provide local settings in json format",
+    long_description=README,
+    url="http://github.com/isotoma/django-json-settings",
+    author="Doug Winter",
+    author_email="doug.winter@isotoma.com",
+)
